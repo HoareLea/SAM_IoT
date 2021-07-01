6ac0d15b41190b42f9e7dddf1907af747543c871
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-07-01 17:02:35 +0100

    Initial Classes (WIP)

---
+++ SAM_IoT/SAM.Core.IoT/Classes/Record.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace SAM.Core.IoT
+{
+    public class Record : IJSAMObject
+    {
+        private long ticks;
+        private ParameterSet parameterSet;
+
+        public Record(Record record)
+        {
+            if (record == null)
+            {
+                return;
+            }
+
+            ticks = record.ticks;
+            parameterSet = record.parameterSet?.Clone();
+        }
+
+        public Record(DateTime dateTime)
+        {
+            ticks = dateTime.Ticks;
+            parameterSet = new ParameterSet(string.Empty);
+        }
+
+        public bool Add(string name, double value)
+        {
+            return Add(name, value as object);
+        }
+
+        public bool Add(string name, string value)
+        {
+            return Add(name, value as object);
+        }
+
+        public bool Add(string name, int value)
+        {
+            return Add(name, value as object);
+        }
+
+        public bool Add(string name, bool value)
+        {
+            return Add(name, value as object);
+        }
+
+        public bool Add(string name, long value)
+        {
+            return Add(name, value as object);
+        }
+
+        public bool Add(string name, DateTime value)
+        {
+            return Add(name, value as object);
+        }
+
+        private bool Add(string name, object value)
+        {
+            if(name == null)
+            {
+                return false;
+            }
+
+            if(parameterSet == null)
+            {
+                parameterSet = new ParameterSet(string.Empty);
+            }
+
+            return parameterSet.Add(name , value as dynamic);
+        }
+
+        public bool FromJObject(JObject jObject)
+        {
+            if(jObject == null)
+            {
+                return false;
+            }
+
+            if(jObject.ContainsKey("Ticks"))
+            {
+                ticks = jObject.Value<long>("Ticks");
+            }
+
+            if (jObject.ContainsKey("ParameterSet"))
+            {
+                parameterSet = new ParameterSet(jObject.Value<JObject>("ParameterSet"));
+            }
+
+            return true;
+        }
+
+        public JObject ToJObject()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("_type", Core.Query.FullTypeName(this));
+            jObject.Add("Ticks", ticks);
+
+            if (parameterSet != null)
+                jObject.Add("ParameterSet", parameterSet.ToJObject());
+
+            return jObject;
+        }
+    }
+}
---
+++ SAM_IoT/SAM.Core.IoT/Query/Response.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SAM.Core.IoT
+{
+    public static partial class Query
+    {
+        public static async Task<JObject> Response(string uri, TimeSpan timeout)
+        {
+            if(string.IsNullOrWhiteSpace(uri))
+            {
+                return null;
+            }
+
+            JObject result = null;
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.ConnectionClose = true;
+                httpClient.Timeout = timeout;
+
+                using (CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(timeout))
+                {
+                    cancellationTokenSource.CancelAfter(timeout);
+
+                    HttpResponseMessage httpResponseMessage = null;
+                    try
+                    {
+                        httpResponseMessage = await httpClient.GetAsync(uri, cancellationTokenSource.Token);
+                    }
+                    catch (TaskCanceledException taskCanceledException)
+                    {
+                        httpResponseMessage = null;
+
+                        httpClient.CancelPendingRequests();
+                    }
+                    catch (Exception exception)
+                    {
+                        httpResponseMessage = null;
+
+                        httpClient.CancelPendingRequests();
+                    }
+
+                    if (httpResponseMessage != null && httpResponseMessage.StatusCode == HttpStatusCode.OK)
+                    {
+                        try
+                        {
+                            string json = await httpResponseMessage.Content.ReadAsStringAsync();
+                            result = JObject.Parse(json);
+                        }
+                        catch
+                        {
+                            result = null;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static async Task<JObject> Response(string uri)
+        {
+            return await Response(uri, TimeSpan.FromSeconds(5));
+        }
+    }
+}
